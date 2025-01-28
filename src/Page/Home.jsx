761dbfb379c1f5637176d4c48760c9f5f29f6877
whoishasan <uniquest.hasan@gmail.com>
2025-01-28 22:12:16 +0600

    home done

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import CallToAction from "../components/CallToAction";
+import Faqs from "../components/faqs";
+import FeaturesSection from "../components/FeaturesSection";
+import HeroSection from "../components/HeroSection";
+import StatsSection from "../components/StatsSection";
+import Testimonials from "../components/Testimonials";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <HeroSection />
+      <StatsSection />
+      <FeaturesSection />
+      <Testimonials />
+      <CallToAction />
+      <Faqs />
+    </div>
+  );
 };
 
 export default Home;
